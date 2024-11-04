cc9a7f29811ac84660756f6ce9395bcb84c50a46
ball0803 <hackball08@gmail.com>
2024-11-04 14:59:38 +0700

    modify lsp config and snippets with super tab keybind

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.o.undofile = true
-vim.opt.shell = vim.fn.executable('pwsh') == 1 and 'pwsh' or 'powershell'
-vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-vim.opt.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-vim.opt.shellquote = ''
-vim.opt.shellxquote = ''
+vim.opt.shiftwidth = 4
+vim.opt.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
+vim.opt.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+vim.opt.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,82 @@
+return {
+
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+        opts = {
+            ensure_installed = {
+                "typescript-langauge-server",
+                "rust-analyzer",
+                "stylua",
+                "shellcheck",
+                "shfmt",
+                "flake8",
+                "gopls",
+            },
+        },
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local on_attach = require("plugins.lspconfig.lspconfig").on_attach
+            local util = require("lspconfig/util")
+            local lspconfig = require("lspconfig")
+
+            lspconfig.html.setup({
+                capabilities = capabilities,
+            })
+
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+                init_options = {
+                    preferences = {
+                        disableSuggestions = true,
+                    },
+                },
+            })
+
+            lspconfig.gopls.setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+                cmd = { "gopls" },
+                filetypes = { "go", "gomod", "gowork", "gotmpl" },
+                root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+            })
+
+            lspconfig.rust_analyzer.setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+                filetypes = { "rust" },
+                root_dir = util.root_pattern("Cargo.toml"),
+                settings = {
+                    ["rust-analyzer"] = {
+                        cargo = {
+                            allFeatures = true,
+                        },
+                    },
+                },
+            })
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
+        end,
+    },
+}
